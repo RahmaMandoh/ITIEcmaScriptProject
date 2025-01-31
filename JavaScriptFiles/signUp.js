477bbf4e622e1c27a8de79f0465e669b04db9ae0
aya-bayoumi <UGS.141230@ci.suez.edu.eg>
2025-01-31 02:01:59 +0200

    added time out page and popup in sign up page

--- JavaScriptFiles/signUp.js
+++ JavaScriptFiles/signUp.js
@@ -20,6 +20,9 @@ var errorMessageEmail = document.getElementById("errorMessageEmail");
 var errorMessagePassword = document.getElementById("errorMessagePassword");
 var errorMessageConf = document.getElementById("errorMessageConf");
 var userArray = JSON.parse(localStorage.getItem("Users")) || [];
+const popup = document.querySelector('.sticky-note');
+const container = document.querySelector('.sticky-container');
+
 
 btn.addEventListener("click", function(event){
     event.preventDefault();
@@ -56,14 +59,36 @@ btn.addEventListener("click", function(event){
             userArray.push(user);
             console.log(userArray);
             localStorage.setItem("Users", JSON.stringify(userArray));
-            alert("User information saved successfully!");
+            // alert("User information saved successfully!");
+            popup.innerHTML =`User Successfully Saved!
+            <svg width="200" height="200" viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
+                <path d="M20 100 L80 160 L180 40" stroke="green" stroke-width="9" fill="transparent" stroke-linecap="round" stroke-linejoin="round">
+                    <animate attributeName="stroke-dasharray" from="0, 300" to="300, 0" dur="1.5s" fill="freeze"/>
+                </path>
+                
+                <circle cx="180" cy="40" r="5" fill="black">
+                    <animateMotion dur="1.5s" fill="freeze" path="M20 100 L80 160 L180 40" keyPoints="0;1" keyTimes="0;1" />
+                </circle>
+            </svg>`
+            popup.classList.add('active');
+            container.classList.add('active');
+            setTimeout(() => {
             window.location.replace("../HTMLFiles/signInhtml.html");
+                
+            }, 2000);
         }else{
             console.log("User is already saved at the local storage");
+            popup.textContent = "This user already exists !";
+            popup.classList.add('active');
+            container.classList.add('active');
+            setTimeout(() => {
+                popup.classList.remove('active');
+                container.classList.remove('active');
+            }, 3000);
         }
         
     } else {
-        alert("Please fix the errors in the form before submitting.");
+        //alert("Please fix the errors in the form before submitting.");
     }
     
     console.log("Sign Up button clicked!");
